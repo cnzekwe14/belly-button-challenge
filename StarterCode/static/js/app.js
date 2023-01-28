11387e2df2ce24ca4b81f87034118f92a9cea778
Christina <cnzekwe14@yahoo.com>
2023-01-28 07:15:30 -0500

    copy

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,34 +1,18 @@
 // read in file
+
+let drop1 = d3.select("#selDataset");
+
 var url ="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-d3.json(url).then(function(x){
-   let table1 = [];
-    for (let i=0;i<x.samples.length,i++;) {
-    
-    var people = x.samples[i];
-    table1.push(people)
-    }
-    // let samps = x.samples.otu_ids
-    console.log(x.samples[1].id); 
+d3.json(url).then(function(data){
+   let row = data;
+   for(let i = 0;i<row.samples.length;i++){
+    d3.selectAll()
 
-
-let drop1 = d3.select(".selDataset");
-
-function Handclick (){
-    console.log(d3.event.target);
-};
-
-drop1.on("optionchanged",Handclick);
-let table2 = [];
-drop1.on("optionchanged", function(){
-    for (let p=0; p<x.samples.length,p++;) {
-        let row = x.samples[i].id;
-        table2.push(row);
-        }
-    console.log(table2);
+   }
 });
 
-});
+
 
 
 
